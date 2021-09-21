6fa74df424d82f6e4fe74e9abbea9183dced388f
Jake O'Shaughnessy <oshaughnessyj@msoe.edu>
2021-09-21 18:55:58 -0500

    Fixed bug with get_os()

--- paleofetch.c
+++ paleofetch.c
@@ -161,6 +161,7 @@ static char *get_os() {
 
     while (getline(&line, &len, os_release) != -1) {
         if (sscanf(line, "NAME=\"%[^\"]+", name) > 0) break;
+        if (sscanf(line, "NAME=%[^\n]+", name) > 0) break;
     }
 
     free(line);
